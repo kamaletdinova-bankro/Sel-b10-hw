53cdfe84be4bbc1bd91b7f104dbd32a44aebe1d1
Elmira Kamaletdinova <e.kamaletdinova@bankro.tech>
2020-03-18 23:51:49 +0400

    Ex 17 attemp 1

--- Tests/LogTest.cs
+++ Tests/LogTest.cs
@@ -1,20 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
 
 namespace Sel_b10_hw
 {
     [TestFixture]
     public class LogTest
     {
-        private IWebDriver driver;
+        private RemoteWebDriver driver;
 
         [SetUp]
         public void CreateDriver()
         {
-            driver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+            options.SetLoggingPreference(LogType.Browser, LogLevel.All);
+            var service = ChromeDriverService.CreateDefaultService();
+            service.EnableVerboseLogging = true;
+            driver = new ChromeDriver(service, options);
         }
         [TearDown]
         public void StopDriver()
@@ -31,9 +36,38 @@ namespace Sel_b10_hw
 
             driver.Url = @"http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1/";
 
-            driver.FindElement(By.ClassName("button")).Click();
+            var folders = driver.FindElements(By.CssSelector("td>a[href*='doc=catalog']"));
+            var foldersLinks = GetLinksList(folders);            
+            var log = driver.Manage().Logs.GetLog(LogType.Browser);
 
-           
+            foreach (var folderLink in foldersLinks)
+            {
+                driver.Url = folderLink;
+                folders = driver.FindElements(By.CssSelector("td>a[href*='doc=catalog']"));
+                foldersLinks = GetLinksList(folders);
+
+                var products = driver.FindElements(By.CssSelector("td>a[href*=product]"));
+                if (products.Count > 0)
+                {
+                    for(int i=0; i < products.Count; i++)
+                    {
+                        products[i].Click();
+                        Assert.That(log.Count == 0);
+                        driver.FindElement(By.CssSelector("button[name=cancel]")).Click();
+                        products = driver.FindElements(By.CssSelector("td>a[href*=product]"));
+                    }
+                }
+            } 
+        }
+
+        private HashSet<string> GetLinksList(ReadOnlyCollection<IWebElement> elements)
+        {
+            var links = new HashSet<string>();
+            foreach(var element in elements)
+            {
+                links.Add(element.GetAttribute("href"));
+            }
+            return links;
         }
     }
 }
